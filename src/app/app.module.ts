3ea4a605ccb8d9f29ea43b81fd655a9405a5d4b8
Walid <walid@griffinkenya.com>
2021-09-09 14:59:35 +0200

    Scaffolded the app - cleaned boilerplate - set folders and finished layout and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainViewComponent } from './containers/main-view/main-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    NavbarComponent,
+    MainViewComponent
   ],
   imports: [
     BrowserModule,
